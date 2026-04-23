a5dbef8856258c63b4afb3c721b8522b4a9b16bd
PabloD6003 <pablodavidgomezv@gmail.com>
2026-04-22 23:32:59 -0600

    Detalles saludo

--- trivia-react-vite/src/components/SaludoPablo.jsx
+++ trivia-react-vite/src/components/SaludoPablo.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import "./SaludoPablo.css";
 
 export default function SaludoPablo() {
-    const [name, setNombre] = useState("Pablo");
+    const [name, setNombre] = useState("Hola");
 
     return (
         <section className="saludo-pablo">
-            <h2 className="saludos_title">¡Hola, {name}!</h2>
+            <h2 className="saludos_title">¡Hola {name}!</h2>
             <p className="saludos_extra">¡Bienvenido a mi primer proyecto en React!</p>
 
         </section>
